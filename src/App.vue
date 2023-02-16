7933c8aeac918ee75fd14f6dae2774613e0b43a9
Renato Martins <rmartins6694@gmail.com>
2023-02-16 15:31:36 -0300

    add new divs about weather

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <div id="app">
     <main>
+
       <div class="search-box">
         <input type="text" class="search-bar" placeholder="Search..."/>
       </div>
+
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Northampton, UK</div>
+          <div class="date">Monday 20 January 2020</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">9°</div>
+          <div class="weather">Rain</div>
+        </div>
+      </div>
+
     </main>
   </div>
   <h1>hello</h1>
@@ -29,7 +42,7 @@ export default {
   padding: 0;
   box-sizing: border-box;
 
-};
+}
 
 body {
   font-family: 'montserrat', sans-serif;
@@ -37,5 +50,45 @@ body {
 
 #app {
   background-image: url('./assets/cold-bg.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+
+  color: #313131;
+  font-size: 20px;
+
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
 }
 </style>
